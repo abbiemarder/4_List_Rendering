48ed46b94a05917b26c6cad9a8d8cf9fc24c83a0
abbiemarder <abbiemarder@gmail.com>
2024-04-30 11:31:32 -0400

    final

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const app = Vue.createApp({
-  data () {
+  data() {
     return {
-      product: 'Socks',
-      image: './assets/images/socks_blue.jpg',
-      inventory: 9,
-      onSale: false
-
-    }
-  }
-})
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      sizes: ["Small", "Medium", "Large"],
+      variants: [
+        { id: 1111, color: "green" },
+        { id: 1112, color: "blue" },
+      ],
+    };
+  },
+});
